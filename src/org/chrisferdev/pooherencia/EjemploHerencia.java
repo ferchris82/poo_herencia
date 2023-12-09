15e5d7badc621445db2d8e4a01b335b18094a721
Christian <ferchris82@hotmail.com>
2023-12-09 15:58:45 -0500

    Versión 0.1

---
+++ src/org/chrisferdev/pooherencia/EjemploHerencia.java
@@ -0,0 +1,18 @@
+package org.chrisferdev.pooherencia;
+
+public class EjemploHerencia {
+    public static void main(String[] args) {
+        Alumno alumno = new Alumno();
+        alumno.setNombre("Andres");
+        alumno.setApellido("Guzman");
+        Profesor profesor = new Profesor();
+        profesor.setNombre("Luci");
+        profesor.setApellido("Perez");
+        profesor.setAsignatura("Matemáticas");
+
+        System.out.println(alumno.getNombre() + " " + alumno.getApellido());
+        System.out.println("Profesor " + profesor.getAsignatura()
+                + ": " + profesor.getNombre()
+                + " " + profesor.getApellido());
+    }
+}
